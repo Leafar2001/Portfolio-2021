964fffe017d3af1d20510e1b1c62847ac8656f33
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2021-12-03 17:26:20 +0100

    optimized images + removed caching temporarily

--- serviceworker.js
+++ serviceworker.js
@@ -1,48 +1,48 @@
-const cacheName = "cache-v1";
-const appFiles = [
-  "./index.html",
-  "./manifest.webmanifest",
-  "./src/css/main.min.css",
-  "./src/js/main.js",
-];
+// const cacheName = "cache-v1";
+// const appFiles = [
+//   "./index.html",
+//   "./manifest.webmanifest",
+//   "./src/css/main.min.css",
+//   "./src/js/main.js",
+// ];
 
 self.addEventListener("install", (installing) => {
-  installing.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(appFiles);
-    })
-  );
+  // installing.waitUntil(
+  //   caches.open(cacheName).then((cache) => {
+  //     return cache.addAll(appFiles);
+  //   })
+  // );
 });
 
 self.addEventListener("activate", (activating) => {});
 
 self.addEventListener("fetch", (fetching) => {
-  fetching.respondWith(
-    caches.match(fetching.request.url).then((response) => {
-      console.log("Service Worker: Fetching resource " + fetching.request.url);
-      return (
-        response ||
-        fetch(fetching.request)
-          .then((response) => {
-            console.log(
-              "Service Worker: Resource " +
-                fetching.request.url +
-                " not available in cache"
-            );
-            return caches.open(cacheName).then((cache) => {
-              console.log(
-                "Service Worker: Caching (new) resource " + fetching.request.url
-              );
-              cache.put(fetching.request, response.clone());
-              return response;
-            });
-          })
-          .catch(function () {
-            console.log("Service Worker: Fetching online failed.");
-          })
-      );
-    })
-  );
+  // fetching.respondWith(
+  //   caches.match(fetching.request.url).then((response) => {
+  //     console.log("Service Worker: Fetching resource " + fetching.request.url);
+  //     return (
+  //       response ||
+  //       fetch(fetching.request)
+  //         .then((response) => {
+  //           console.log(
+  //             "Service Worker: Resource " +
+  //               fetching.request.url +
+  //               " not available in cache"
+  //           );
+  //           return caches.open(cacheName).then((cache) => {
+  //             console.log(
+  //               "Service Worker: Caching (new) resource " + fetching.request.url
+  //             );
+  //             cache.put(fetching.request, response.clone());
+  //             return response;
+  //           });
+  //         })
+  //         .catch(function () {
+  //           console.log("Service Worker: Fetching online failed.");
+  //         })
+  //     );
+  //   })
+  // );
 });
 
 self.addEventListener("push", (pushing) => {});
