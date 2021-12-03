1ff28444e6be2cadd6018576eb3e0acecb4f159f
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2021-12-03 13:12:30 +0100

    update serviceworker & swapped skatespot with planegame

--- serviceworker.js
+++ serviceworker.js
@@ -7,21 +7,16 @@ const appFiles = [
 ];
 
 self.addEventListener("install", (installing) => {
-  console.log("Service Worker: I am being installed, hello world!");
   installing.waitUntil(
     caches.open(cacheName).then((cache) => {
-      console.log("Service Worker: Caching important offline files");
       return cache.addAll(appFiles);
     })
   );
 });
 
-self.addEventListener("activate", (activating) => {
-  console.log("Service Worker: All systems online, ready to go!");
-});
+self.addEventListener("activate", (activating) => {});
 
 self.addEventListener("fetch", (fetching) => {
-  console.log("Service Worker: User threw a ball, I need to fetch it!");
   fetching.respondWith(
     caches.match(fetching.request.url).then((response) => {
       console.log("Service Worker: Fetching resource " + fetching.request.url);
@@ -50,8 +45,4 @@ self.addEventListener("fetch", (fetching) => {
   );
 });
 
-// self.addEventListener("push", (pushing) => {
-//   console.log(
-//     "Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :("
-//   );
-// });
+self.addEventListener("push", (pushing) => {});
