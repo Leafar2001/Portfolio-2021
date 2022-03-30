dea9a26d9ed7280d2c528f3ce197fa9f0d690f2d
Luc Swinkels <luc.swinkels@live.nl>
2022-03-30 13:56:09 +0200

    added offset to smoothscroll, added sidenav highlighting based on section

--- src/js/main.js
+++ src/js/main.js
@@ -157,3 +157,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
 //   // Attach the handler
 //   ele.addEventListener("mousedown", mouseDownHandler);
 // });
+
+// Internship sidenav current section indicator
+const sections = document.querySelectorAll("section");
+console.log(sections);
+const navLi = document.querySelectorAll(".side--nav ul li a");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    const href = li.getAttribute("href").substring(1);
+    if (href === current) {
+      li.classList.add("active");
+    }
+  });
+});
